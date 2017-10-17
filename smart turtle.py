f03fcbc383010a1a0bbb3408f9a1df2c452dc725
Simon Yeung <simonysck0117@gmail.com>
2017-10-17 09:41:32 -0400

    Added Fetch Position Info Will now update quantity and avg_price per certain time interval quantity for rollover avg_price for calcuating win/loss which affects the portfolio value

--- smart turtle.py
+++ smart turtle.py
@@ -36,12 +36,12 @@ def initialize(context):
     ]
 
     context.required_information = [
-        'cfutures',
+        'cfutures', #ok
         'keep track or trade', # whether we are keeping track of winning or losing or really trading
         'type of breakout',
         'scale-in stage',
-        'quantity',
-        'average price',
+        'quantity', #do next
+        'avg price', #do next
         'initial price',
         'second price',
         'third price',
@@ -59,8 +59,10 @@ def initialize(context):
         'profit of last trade',
         'was last trade winning',
     ]
-    context.master_table= pd.DataFrame(data = None, index = context.symbols, columns = context.required_information)
-    context.master_table'scale-in price'] = 0
+
+    #creating a Master Table
+    context.MT= pd.DataFrame(data = None, index = context.symbols, columns = context.required_information)
+    context.MT'scale-in price'] = 0
 
     context.cfutures = {symbol: continuous_future(symbol , offset = 0, roll = 'calendar' , adjustment = 'mul') for symbol in context.symbols}
 
@@ -133,6 +135,13 @@ def initialize(context):
 
     total_minutes = 6*60+30
     for i in range(30, total_minutes, 30):
+        schedule_function(
+            fetch_position_info,
+            date_rules.every_day(),
+            time_rules.market_open(minutes=i)
+            False
+        )
+
         schedule_function(
             compute_average_true_ranges,
             date_rules.every_day(),
@@ -210,7 +219,7 @@ def validate_prices(context, data):
     )
 
     context.tradable_symbols = validated_markets
-    context.master_table.loc[dropped_markets] = None
+    context.MT.loc[dropped_markets] = None
 
     log.info(
         'Null prices for %s. Dropped.'
@@ -223,20 +232,20 @@ def compute_breakout_price(context, data):
     Compute high and low for breakout price
     """
     for sym in context.tradable_symbols:
-        context.master_table.loc[sym]['strat 1 long breakout price'] = context.prices\
+        context.MT.loc[sym]['strat 1 long breakout price'] = context.prices\
             .loc[sym, 'high']\
             [-context.strat_one_breakout-1:-1]\
             .max()
-        context.master_table.loc[sym]['strat 2 long breakout price'] = context.prices\
+        context.MT.loc[sym]['strat 2 long breakout price'] = context.prices\
             .loc[sym, 'high']\
             [-context.strat_two_breakout-1:-1]\
             .max()
         
-        context.master_table.loc[sym]['strat 1 short breakout price'] = context.prices\
+        context.MT.loc[sym]['strat 1 short breakout price'] = context.prices\
             .loc[sym, 'low']\
             [-context.strat_one_breakout-1:-1]\
             .min()
-        context.master_table.loc[sym]['strat 2 short breakout price'] = context.prices\
+        context.MT.loc[sym]['strat 2 short breakout price'] = context.prices\
             .loc[sym, 'low']\
             [-context.strat_two_breakout-1:-1]\
             .min()
@@ -258,8 +267,8 @@ def compute_exit_price(context,data):
         [-context.strat_two_exit-1:-1].max()
     
     for sym in context.tradable_symbols:
-        context.master_table['exit price'] = exit_prices[sym]\
-            [context.master_table[sym]['type of breakout']]
+        context.MT['exit price'] = exit_prices[sym]\
+            [context.MT[sym]['type of breakout']]
 
 
 def get_contracts(context, data):
@@ -282,7 +291,7 @@ def get_contracts(context, data):
     contracts = contracts.rename_axis(lambda k: k.root_symbol, axis=0)
 
     for sym in context.tradable_symbols
-    context.master_table.loc[sym]['current contract'] = contracts[sym]
+    context.MT.loc[sym]['current contract'] = contracts[sym]
 
 
 def check_rollover(context, data):
@@ -290,14 +299,14 @@ def check_rollover(context, data):
     see if the contract has rollovered
     """
     for sym in context.tradable_symbols:
-        current_auto_close_date = context.master_table[sym]['current contract'].auto_close_date
+        current_auto_close_date = context.MT[sym]['current contract'].auto_close_date
                
-        if current_auto_close_date != context.master_table[sym]['auto close date']:
-                if context.master_table.loc[sym]['scale-in stage'] != 0:
+        if current_auto_close_date != context.MT[sym]['auto close date']:
+                if context.MT.loc[sym]['scale-in stage'] != 0:
                     price = data.current(context.cfutures[sym], 'price')
                     order_identifier = order(
-                        context.master_table.loc[sym]['current contract'],
-                        context.master_table.loc[sym]['total quantity'],
+                        context.MT.loc[sym]['current contract'],
+                        context.MT.loc[sym]['quantity'],
                         style = LimitOrder(price)
                     )
 
@@ -308,14 +317,29 @@ def check_rollover(context, data):
                         'rollover %s %i@%.2f'
                         %(
                             sym,
-                            context.master_table.loc[sym]['total quantity'],
+                            context.MT.loc[sym]['quantity'],
                             price
                         )
                     )
     
-        context.master_table[sym]['auto close date'] = current_auto_close_date
+        context.MT[sym]['auto close date'] = current_auto_close_date
 
 # repeating functions
+
+def fetch_position_info(context, data):
+    context.MT['quantity'].mask(
+        context.MT['keep track or trade'] == 'trade',
+        context.portfolio.positions[context.MT['current contract']].amount,
+        inplace=True
+    )
+
+    context.MT['avg price'].mask(
+        context.MT['keep track or trade'] == 'trade',
+        context.portfolio.positions[context.MT['current contract']].cost_basis,
+        inplace=True
+    )
+
+           
 
 def compute_average_true_ranges(context, data):
     """
@@ -326,7 +350,7 @@ def compute_average_true_ranges(context, data):
     moving_average = context.strat_one_breakout
 
     for sym in context.tradable_symbols:
-        context.master_table[sym]['ATR'] = ATR(
+        context.MT[sym]['ATR'] = ATR(
             context.prices.loc[sym, 'high'][-rolling_window:],
             context.prices.loc[sym, 'low'][-rolling_window:],
             context.prices.loc[sym, 'close'][-rolling_window:],
@@ -340,16 +364,16 @@ def compute_trade_sizes(context, data):
     """
     for sym in context.tradable_symbols:
         dollar_volatility = context.contracts[sym].multiplier\
-            * context.master_table[sym]['ATR']
+            * context.MT[sym]['ATR']
 
-            context.master_table[sym]['unit size'] = int(context.tradable_capital/dollar_volatility)
+            context.MT[sym]['unit size'] = int(context.tradable_capital/dollar_volatility)
 
 def update_risks(context,data):
     """
     Calculate long and short risk
     """
-    long_risk_numbers = [x for x in context.master_table['scale-in stage'] if x > 0]
-    short_risk_numbers = [x for x in context.master_table['scale-in stage'] if x < 0]
+    long_risk_numbers = [x for x in context.MT['scale-in stage'] if x > 0]
+    short_risk_numbers = [x for x in context.MT['scale-in stage'] if x < 0]
 
     context.long_risk = sum(long_risk_numbers)
     context.short_risk = sum(short_risk_numbers)
